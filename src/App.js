658cc37e9de3c4736a8e951a8ac54a569f008a51
Jeongho Jeong <jeongho.jeong12@gmail.com>
2021-10-12 18:56:01 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,19 +4,29 @@ import { Canvas } from '@react-three/fiber'
 import { Planes } from './Planes'
 
 export default function App() {
+  const planes = React.useRef()
+
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Canvas
-        style={{ backgroundColor: 'black' }}
-        onCreated={(state) => {
-          // const gl = state.gl.getContext()
-          // console.log()
-          // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
-          // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-          // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAX_FILTER, gl.NEAREST)
-        }}>
-        <Planes />
-      </Canvas>
-    </Suspense>
+    <div
+      style={{
+        height: '100%'
+      }}
+      onClick={() => {
+        planes?.current?.create()
+      }}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Canvas
+          style={{ backgroundColor: 'black' }}
+          onCreated={(state) => {
+            // const gl = state.gl.getContext()
+            // console.log()
+            // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+            // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+            // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAX_FILTER, gl.NEAREST)
+          }}>
+          <Planes ref={planes} />
+        </Canvas>
+      </Suspense>
+    </div>
   )
 }
--- src/Planes.js
+++ src/Planes.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import { useFrame, useLoader } from '@react-three/fiber'
-import React, { useRef } from 'react'
+import React, { useImperativeHandle, useRef } from 'react'
 import Img from './heart.png'
 
 const NUM_ITEMS = 10
@@ -9,6 +9,10 @@ function getEmptyArray(size) {
   return new Array(size).fill()
 }
 
+const POSITION_RANGE = 4
+const X_SPEED_RANGE = 1.5
+const ROTATION_SPEED_RANGE = 1
+
 /**
  * TODOs
  * - [x] 매 프레임마다 위치 바뀌게 하기
@@ -16,32 +20,64 @@ function getEmptyArray(size) {
  * - [x] 각 plane에 다른 opacity 적용하기
  * - [x] 매 프레임마다 opacity 바뀌게 하기
  * - [x] 투명 텍스처 적용하기
+ * - [x] 점점 사라지게 하기
+ * - [x] y축 가속 운동 처리
+ * - [ ] 회전 처리
+ * - [ ] 여러 이미지 중 하나 랜덤 어사인해 사용
  */
-export function Planes() {
+function _Planes(_, ref) {
   const [texture] = useLoader(THREE.TextureLoader, [Img])
   const meshRef = useRef()
+  const shaderMaterialRef = useRef()
   const positionArray = useRef(Float32Array.from(getEmptyArray(NUM_ITEMS * 3).map(() => 0))).current
-  // const timeArray = useRef(Uint32Array.from(getEmptyArray(NUM_ITEMS)).map(() => 0)).current
+  const timeArray = useRef(Float32Array.from(getEmptyArray(NUM_ITEMS)).map(() => 0)).current
+  const xSpeedArray = useRef(Float32Array.from(getEmptyArray(NUM_ITEMS)).map(() => 0)).current
+  const rotationSpeedArray = useRef(Float32Array.from(getEmptyArray(NUM_ITEMS)).map(() => 0)).current
+  const count = useRef(0)
+  const now = useRef(0)
+
+  useImperativeHandle(ref, () => ({
+    create() {
+      const idx = count.current
+      timeArray[idx] = now.current
+      positionArray[idx * 3] = (Math.random() - 0.5) * POSITION_RANGE
+      positionArray[idx * 3 + 1] = (Math.random() - 0.5) * POSITION_RANGE
+      positionArray[idx * 3 + 2] = 0
+
+      xSpeedArray[idx] = (Math.random() - 0.5) * X_SPEED_RANGE
+      rotationSpeedArray[idx] = (Math.random() - 0.5) * ROTATION_SPEED_RANGE
+
+      count.current = (idx + 1) % NUM_ITEMS
+      if (meshRef.current) {
+        meshRef.current.geometry.attributes.planePos.needsUpdate = true
+        meshRef.current.geometry.attributes.startTime.needsUpdate = true
+        meshRef.current.geometry.attributes.xSpeed.needsUpdate = true
+        meshRef.current.geometry.attributes.rotationSpeed.needsUpdate = true
+      }
+    }
+  }))
 
   useFrame((state) => {
     const t = state.clock.getElapsedTime()
-    for (let i = 0; i < NUM_ITEMS; i += 1) {
-      const speed = 1 + i * 0.01
-      positionArray[i * 3] = Math.sin(t * speed) + i - NUM_ITEMS / 2
-      positionArray[i * 3 + 1] = Math.cos(t * speed) + i - NUM_ITEMS / 2
+    now.current = t
+    if (shaderMaterialRef.current) {
+      shaderMaterialRef.current.uniforms.time.value = t
     }
-    meshRef.current.geometry.attributes.planePos.needsUpdate = true
   })
 
   return (
     <instancedMesh ref={meshRef} args={[null, null, NUM_ITEMS]}>
       <planeGeometry args={[0.5, 0.5, 1]}>
         <instancedBufferAttribute attachObject={['attributes', 'planePos']} args={[positionArray, 3]} />
+        <instancedBufferAttribute attachObject={['attributes', 'startTime']} args={[timeArray, 1]} />
+        <instancedBufferAttribute attachObject={['attributes', 'xSpeed']} args={[xSpeedArray, 1]} />
+        <instancedBufferAttribute attachObject={['attributes', 'rotationSpeed']} args={[rotationSpeedArray, 1]} />
       </planeGeometry>
       <shaderMaterial
-        ref={(r) => {
-          if (r) {
-            r.transparent = true
+        ref={(ref) => {
+          if (ref) {
+            ref.transparent = true
+            shaderMaterialRef.current = ref
           }
         }}
         attach="material"
@@ -52,23 +88,40 @@ export function Planes() {
               tex: {
                 type: 't',
                 value: texture
+              },
+              time: {
+                type: 'float',
+                value: 0
               }
             },
             vertexShader: `
+                uniform float time;
                 attribute vec3 planePos;
+                attribute float startTime;
+                attribute float xSpeed;
                 varying vec2 vUv;
+                varying float alpha;
 
                 void main() {
-                  gl_Position = projectionMatrix * modelViewMatrix * vec4(position + planePos, 1.0);
+                  float v0 = 1.2;
+                  float a = -1.8;
+                  float t = time - startTime;
+                  float y = v0 * t + 0.5 * a * t * t;
+                  float x = xSpeed * t;
+                  vec4 pos = vec4(position + planePos, 1.0) + vec4(x, y, 0, 0);
+                  gl_Position = projectionMatrix * modelViewMatrix * pos;
                   vUv = uv;
+                  alpha = 1.0 - clamp(t * 0.7, 0.0, 1.0);
                 }
               `,
             fragmentShader: `
                 varying vec2 vUv;
+                varying float alpha;
                 uniform sampler2D tex;
 
                 void main() {
                   gl_FragColor = texture2D(tex, vUv);
+                  gl_FragColor.a = alpha * gl_FragColor.a;
                 }
               `
           }
@@ -77,3 +130,5 @@ export function Planes() {
     </instancedMesh>
   )
 }
+
+export const Planes = React.forwardRef(_Planes)
